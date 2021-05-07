951ad91bb5c3eff078f9367fd435b36528c6aa79
DabTheMatt <71525403+DabTheMatt@users.noreply.github.com>
2021-05-07 22:53:49 +0200

    setting up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./components/Home/Home";
+import Globals from "./components/global/GlobalStyles";
+
+function App() {
+  return (
+    <>
+    <Globals/>
+    <Home/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/styles.js
@@ -0,0 +1,12 @@
+import { css } from "styled-components";
+
+export const setColor = {
+    mainBlack: "#011b28",
+    mainWhite: "#EEF7FB",
+    lightAccent: "#011b28",
+    darkAccent: "#011b28"
+};
+
+export const setFont = {
+    main: "'Roboto', sans-serif;"
+};
