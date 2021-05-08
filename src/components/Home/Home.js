6cbbb53f10992c3a002e7c072751974dae6a6fde
DabTheMatt <71525403+DabTheMatt@users.noreply.github.com>
2021-05-08 20:50:07 +0200

    global styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+import { setColor, setRem, setShadow, setFlex } from "../../styles";
 
 const Home = () => {
     return (
-        <>
-        Hello from Home
-        </>
+        <HomeWrapper>
+            <h1 className="title">Hello from Home</h1> 
+        </HomeWrapper>
     )
 }
+
+const HomeWrapper = styled.div`
+    height: 100vh;
+    background: linear-gradient(45deg, ${setColor.lightAccent}, ${setColor.darkAccent});
+    ${setFlex({})};
+h1{
+    color: ${setColor.mainBlack};
+    font-size: ${setRem(100)};
+    ${setShadow("text", "2px", "white")};
+    font-weight: 100;
+}
+`;
 
 export default Home;
--- src/styles.js
+++ src/styles.js
@@ -3,10 +3,24 @@ import { css } from "styled-components";
 export const setColor = {
     mainBlack: "#011b28",
     mainWhite: "#EEF7FB",
-    lightAccent: "#011b28",
-    darkAccent: "#011b28"
+    lightAccent: "#ffb703",
+    darkAccent: "#fb8500"
 };
 
 export const setFont = {
     main: "'Roboto', sans-serif;"
 };
+
+export const setRem = (number = 16) => {
+    return`${number/16}rem`
+}
+
+export const setShadow = (type="box", size="20px", color="black") => {
+    return `${type}-shadow: ${size} ${size} ${color}`;
+    
+    ;
+}
+
+export const setFlex = ({x="center", y="center"}={}) => {
+    return`display:flex;align-items:${y};justify-content:${x}`
+}
